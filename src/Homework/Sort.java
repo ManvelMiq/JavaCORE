26023e1c5c8e8f8e1470367a45633b516179ca5c
Մանվել Միքայելյան <miqayelyanm390@gmail.com>
2025-10-25 21:26:52 +0400

    Array

--- src/Homework/Sort.java
+++ src/Homework/Sort.java
@@ -6,17 +6,19 @@ public class Sort {
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
-                    int temp = array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
+                    int a = array[i];
+
+                    array[i] = array[j];
+                    array[j] = a;
+
 
                 }
 
             }
 
         }
-        for (int i = 0; i <array.length ; i++) {
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
 
         }
     }
